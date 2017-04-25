2aed6c6f0988c96b3874042b81fc5d94aa1d6592
Howard Tyson <howard@howardtyson.com>
2017-04-25 14:44:58 -0400

    Updating default.pp for service configuration.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,93 +1,220 @@
-class { 'jenkins':
-  config_hash => {'HTTP_PORT' => {'value' => '8092'}},
-  configure_firewall => false,
-}
-/*
 
-include webadmin
-
-require 'apt'
-
+/**
+ * Create a simple non-secured rabbitmq setup for sensu.
+ *
+ * See the sensu-ssl-tool and documentation for (relatively) easy setup instructions.
+ * https://sensuapp.org/docs/0.25/reference/ssl.html#generate-self-signed-openssl-certificates-and-ca
+ */
 class { '::rabbitmq':
-  service_manage    => true,
-  port              => 5672,
+  delete_guest_user => true,
   admin_enable      => true,
-  delete_guest_user => false,
 }
 
 rabbitmq_user { 'sensu':
-  admin             => true,
-  password          => 'boo',
-  provider          => 'rabbitmqctl',
-  require           => Service['rabbitmq-server'],
+  admin    => true,
+  password => 'bar',
 }
-
 rabbitmq_vhost { '/sensu':
   ensure => present,
-  provider => 'rabbitmqctl',
-  require  => Service['rabbitmq-server'],
 }
-
-rabbitmq_user_permissions { 'sensu@sensu':
+rabbitmq_user_permissions { 'sensu@/sensu':
   configure_permission => '.*',
   read_permission      => '.*',
   write_permission     => '.*',
-  require  => [
-    Service['rabbitmq-server'],
+  require              => [
+    Rabbitmq_vhost['/sensu'],
     Rabbitmq_user['sensu'],
-    Rabbitmq_vhost['sensu'],
   ],
 }
 
-rabbitmq_vhost { 'sensu':
-  ensure => present,
-  provider => 'rabbitmqctl',
-}
-
+/**
+ * Install redis-server for use with sensu.
+ */
 package { 'redis-server':
-  ensure => 'installed',
-}->
+  ensure => present,
+}~>
 
 service { 'redis-server':
   ensure => 'running',
   enable => true,
-}->
-   
-package { 'ruby-dev': }->
-
-class { 'sensu':
-  rabbitmq_password => 'boo',
-  rabbitmq_host     => '127.0.0.1',
-  rabbitmq_vhost    => 'sensu',
-  sensu_plugin_version     => '1.1.0',
-  subscriptions     => 'all',
-  install_repo      => true,
-  server            => true,
-  manage_services   => true,
-  manage_user       => true,
-  api               => true,
-  client_address    => $::ipaddress_eth1,
-  # For whatever reason localhost was choking on a fresh install on 14.04.
-  redis_host        => '127.0.0.1',
-  plugins           => [
-    'puppet:///modules/sensu_community_plugins/plugins/system/check-disk.rb',
-    'puppet:///modules/sensu_community_plugins/plugins/system/check-cpu.rb',
-    'puppet:///modules/sensu_community_plugins/plugins/system/check-ram.rb',
-    'puppet:///modules/sensu_community_plugins/plugins/system/check-load.rb',
-    'puppet:///modules/sensu_community_plugins/plugins/system/check-swap-percentage.sh',
-    'puppet:///modules/sensu_community_plugins/plugins/system/load-metrics.rb',
-    'puppet:///modules/sensu_community_plugins/plugins/system/memory-metrics-percent.rb',
-    'puppet:///modules/sensu_community_plugins/plugins/system/disk-usage-metrics.rb',
-  ],
-  use_embedded_ruby => true,
 }
 
+/**
+ * Install the sensu server and client for use on this single server setup.
+ */
+class { '::sensu':
+  install_repo          => true,
+  server                => true,
+  manage_services       => true,
+  manage_user           => true,
+  rabbitmq_password     => 'bar',
+  rabbitmq_vhost        => '/sensu',
+  # Sensu ships with a gem for use in writing plugins, this installs
+  # it with the sensu provider.
+  sensu_plugin_provider => 'sensu_gem',
+  api                   => true,
+  api_user              => 'admin',
+  api_password          => 'secret',
+  client_address        => $::ipaddress_eth1,
+  subscriptions         => ['all'],
+}
 
-sensu::subscription { 'all': }
+/**
+ * Create a simple handler that writes to a file.
+ */
+file { '/var/log/sensu-default-handler.log':
+  ensure => 'file',
+  owner  => 'sensu',
+  mode   => '0555'
+}->
 
 sensu::handler { 'default':
-  command => 'tee /tmp/sensu-notifications.log',
+  command => 'echo $(cat) > /var/log/sensu-default-handler.log',
+  type    => 'pipe',
 }
+
+# An example handler just for this host.
+sensu::subscription { 'sensu-test': }
+# A super simple check for servers with the above subscription.
+sensu::check { 'success':
+  command     => 'echo "Something went right" && /bin/true',
+  handlers    => 'default',
+  subscribers => 'sensu-test',
+}
+
+sensu::check { 'failure':
+  command     => 'echo "Something went wrong" && /bin/false',
+  handlers    => 'default',
+  subscribers => 'sensu-test',
+}
+
+# Install the memory checks plugin from rubygems into the embedded sensu ruby gems.
+package { 'sensu-plugins-memory-checks':
+  provider => 'sensu_gem',
+}->
+# Required to build the vmstat ruby gem.
+package { 'build-essential':
+  ensure => 'present',
+}->
+# Install vmstat, a dependency of one of the memory checks we are using.
+package { 'vmstat':
+  provider => 'sensu_gem',
+}
+
+# Define simple checks from the above definition.
+sensu::check { 'ram':
+  command     => '/opt/sensu/embedded/bin/check-ram.rb',
+  subscribers => 'all',
+  standalone  => false,
+}->
+
+sensu::check { 'swap-percent':
+  command     => '/opt/sensu/embedded/bin/check-swap-percent.rb -w 80 -c 95',
+  subscribers => 'all',
+  standalone  => false,
+}->
+
+# Define memory metrics checks to collect memory information.
+sensu::check { 'memory-metrics':
+  command     => '/opt/sensu/embedded/bin/metrics-memory-percent.rb',
+  type        => 'metric',
+  subscribers => 'all',
+  handlers    => 'influx',
+}->
+
+sensu::check { 'swap-percent-metrics':
+  command     => '/opt/sensu/embedded/bin/metrics-swap-percent.rb',
+  type        => 'metric',
+  subscribers => 'all',
+  handlers    => 'influx',
+  ensure      => 'absent',
+}->
+
+package { 'uchiwa':
+  ensure => present,
+  require => Class['sensu'],
+}
+
+file { '/etc/sensu/uchiwa.json':
+  ensure  => file,
+  content => '
+{
+  "sensu": [
+    {
+      "name": "Site",
+      "host": "127.0.0.1",
+      "port": 4567,
+      "timeout": 5,
+      "user": "admin",
+      "pass": "secret"
+    }
+  ],
+  "uchiwa": {
+    "host": "0.0.0.0",
+    "port": 3000,
+    "interval": 5
+  }
+}',
+  require => Package['uchiwa'],
+}~>
+
+service { 'uchiwa':
+  ensure  => running,
+  require => [ File['/etc/sensu/uchiwa.json'],Package['uchiwa'] ],
+  enable  => true,
+}
+
+# TODO: This requires a two provisions before it works, investigate.
+include ::influxdb::server
+
+class { 'grafana':
+  cfg => {
+    app_mode => 'production',
+    server   => {
+      http_port     => 8090,
+    },
+    database => {
+      type          => 'sqlite3',
+      host          => '127.0.0.1:3306',
+      name          => 'grafana',
+      user          => 'root',
+      password      => '',
+    },
+    users    => {
+      allow_sign_up => false,
+    },
+  },
+}
+
+# Setup vhosts to proxy to the requisite services.
+include nginx
+nginx::resource::vhost { 'uchiwa.drupal-devops.zivtech.com':
+  server_name => ['uchiwa.drupal-devops.zivtech.com'],
+  proxy       => 'http://localhost:3000',
+}
+
+nginx::resource::vhost { 'influxdb.drupal-devops.zivtech.com':
+  server_name => ['influxdb.drupal-devops.zivtech.com'],
+  proxy       => 'http://localhost:8083',
+}
+nginx::resource::vhost { 'grafana.drupal-devops.zivtech.com':
+  server_name => ['grafana.drupal-devops.zivtech.com'],
+  proxy       => 'http://localhost:8090',
+}
+
+nginx::resource::vhost { 'jenkins.drupal-devops.zivtech.com':
+  server_name => ['jenkins.drupal-devops.zivtech.com'],
+  proxy       => 'http://localhost:8080',
+}
+
+nginx::resource::vhost { 'rabbit.drupal-devops.zivtech.com':
+  server_name => ['jenkins.drupal-devops.zivtech.com'],
+  proxy       => 'http://localhost:15672',
+}
+
+include ::jenkins
+
+/*
 
 # We can't use sensu embedded because gems can't be installed inside sensu embedded if they
 # have the same name as a package that is being required.
@@ -111,142 +238,5 @@ sensu::handler { 'influx':
 # ruby provided by sensu.
 package { 'sensu-plugins-influxdb':
   provider => 'sensu_gem',
-}->
-
-sensu::check { 'disk-usage-metrics':
-  type        => 'metric',
-  command     => '/etc/sensu/plugins/disk-usage-metrics.rb',
-  subscribers => 'all',
-  standalone  => false,
-  handlers    => 'influx',
-}
-
-sensu::check { 'memory-metrics':
-  command     => '/etc/sensu/plugins/memory-metrics-percent.rb',
-  type        => 'metric',
-  subscribers => 'all',
-  handlers    => 'influx',
-}
-
-sensu::check { 'load-metrics':
-  command     => '/etc/sensu/plugins/load-metrics.rb',
-  type        => 'metric',
-  subscribers => 'all',
-  handlers    => 'influx',
-}
-
-sensu::check { 'load':
-  command     => '/etc/sensu/plugins/check-load.rb',
-  subscribers => 'all',
-  standalone  => false,
-}
-
-sensu::check { 'cpu':
-  command     => '/etc/sensu/plugins/check-cpu.rb',
-  subscribers => 'all',
-  standalone  => false,
-}
-
-sensu::check { 'ram':
-  command     => '/etc/sensu/plugins/check-ram.rb',
-  subscribers => 'all',
-  standalone  => false,
-}
-
-sensu::check { 'disk':
-  command     => '/etc/sensu/plugins/check-disk.rb',
-  subscribers => 'all',
-  standalone  => false,
-}
-
-sensu::check { 'swap-percent':
-  command     => '/etc/sensu/plugins/check-swap-percentage.sh',
-  subscribers => 'all',
-  standalone  => false,
-}
-
-package { 'uchiwa':
-  ensure  => present,
-  require => Class['sensu'],
-}
-
-file { '/etc/sensu/uchiwa.json':
-  ensure  => present,
-  content => '
-{
-  "sensu": [
-    {
-      "name": "Site1",
-      "host": "localhost",
-      "port": 4567,
-      "timeout": 5,
-      "user": "sensu",
-      "pass": "boo"
-    }
-  ],
-  "uchiwa": {
-    "host": "0.0.0.0",
-    "port": 3000,
-    "interval": 5
-  }
-}',
-  require => [
-    Package['uchiwa'],
-    Class['sensu'],
-  ],
-  notify  => Service['uchiwa'],
-}
-
-service { 'uchiwa':
-  ensure  => running,
-  require => [
-    File['/etc/sensu/uchiwa.json'],
-    Package['uchiwa']
-  ]
-}
-
-class { 'logstash':
-  manage_repo     => true,
-  repo_version    => '1.5',
-}
-
-logstash::configfile { 'main':
-  source => 'puppet:///modules/logstash_config/logstash.conf',
-}
-
-package { 'logstash-contrib':
-  ensure => 'installed',
-}
-
-class { 'elasticsearch':
-  manage_repo  => true,
-  repo_version => '1.5',
-  #java_install => true,
-}
-
-elasticsearch::instance { 'es-01':
-  require => [
-    Class['elasticsearch'],
-    Package['elasticsearch'],
-  ],
-}
-
-elasticsearch::plugin{'mobz/elasticsearch-head':
-  instances  => 'es-01'
-}
-
-class { 'kibana4':
-}
-
-
-class {'influxdb::server':
-}
-
-class { 'grafana':
-  cfg => {
-    server => {
-      http_port => 8081,
-    },
-  },
 }
 */
